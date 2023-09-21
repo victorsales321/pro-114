0f45de835c51b3c079484a1effd4af5e48c33d86
victorsales321 <110502785+victorsales321@users.noreply.github.com>
2023-09-21 19:41:39 -0300

    Add files via upload

---
+++ PRO_1-4_C114_SolucaoDoProjeto-main/test1.py
@@ -0,0 +1,7 @@
+a = 33
+b = 33
+if b > a:
+  print("b é maior que a")
+elif b == a:
+  print("a e b são iguais")
+ 
---
+++ PRO_1-4_C114_SolucaoDoProjeto-main/test2.py
@@ -0,0 +1,7 @@
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emilly")
+my_function("Tobias")
+my_function("Lucas")
+
